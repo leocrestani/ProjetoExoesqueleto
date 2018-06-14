db72f66cf52f5f2404e62bed57ab7cddfc763599
120789 <120789@JBA-LABELET02.unoesc.lan>
2018-06-14 15:40:10 -0300

    Início do projeto

---
+++ index.php
@@ -0,0 +1,13 @@
+<!--Main index do projeto-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Exoesqueleto</title>
+    </head>
+    <body>
+        <?php
+        
+        ?>
+    </body>
+</html>
+
